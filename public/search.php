5a6e717e066e1ec66d919b2d34d601416e90bd1e
Albert Garangou Culebras <albertgarangou@gmail.com>
2022-03-07 19:40:17 +0100

    + search.php plantilla html/css + Logout al search.php + Sessió guardada en text pla per user_id

--- public/search.php
+++ public/search.php
@@ -2,51 +2,51 @@
 ob_start();
 ?>
 <?php
-    if(in_array('carquinyolisSession', $_COOKIE) == false){ #existeix la cookie
-        if($_COOKIE['carquinyolisSession'] == null) { #està plena
-           redirectToLogin();
-        }
+if(in_array('carquinyolisSession', $_COOKIE) == false){ #existeix la cookie
+    if($_COOKIE['carquinyolisSession'] == null) { #està plena
+        redirectToLogin();
     }
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
 
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-        <meta name="authors" content="Albert Garangou">
-        <title>THE GIF CLUB</title>
-        <link rel="stylesheet" href="./css/union.css">
-        <META NAME="robots" CONTENT="nofollow">
-        <META name="description" content="PHP AC1 Proj Web 2">
-        <META name="keywords" content="GIF, AC1, PROJ-WEB-2">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta name="authors" content="Albert Garangou">
+    <title>THE GIF CLUB</title>
+    <link rel="stylesheet" href="./css/union.css">
+    <META NAME="robots" CONTENT="nofollow">
+    <META name="description" content="PHP AC1 Proj Web 2">
+    <META name="keywords" content="GIF, AC1, PROJ-WEB-2">
 
-        <link rel="icon" href="/media/logo.webp"></head>
+    <link rel="icon" href="/media/logo.webp"></head>
 
-    <body>
-        <h1>The GIF CLUB</h1><br>
+<body>
+<h1>The GIF CLUB</h1><br>
 
-        <form method="POST">
-            <label for="search">Search a topic: </label>
-            <input id="search" type="text" placeholder="Mems, FPI open up, urss, pokimon, doramion..." name="search">
-            <button id="search-button" type="submit" value="Send" onclick="search">Find it!</button>
-            <br>
+<form method="POST">
+    <label for="search">Search a topic: </label>
+    <input id="search" type="text" placeholder="Mems, FPI open up, urss, pokimon, doramion..." name="search">
+    <button id="search-button" type="submit" value="Send" onclick="search">Find it!</button>
+    <br>
 
-        </form>
-        <?php
-        if($_POST && isset($_POST['search'])){
-            searchGIF(implode($_POST));
-        }
-        ?>
+</form>
+<?php
+if($_POST && isset($_POST['search'])){
+    searchGIF(implode($_POST));
+}
+?>
 
-        <form method="POST">
-            <input type="submit" name="logout" value=" Logout " onclick="logout">
+<form method="POST">
+    <input type="submit" name="logout" value=" Logout " onclick="logout">
 
-        </form>
-        <?php
-            if($_POST && isset($_POST['logout'])) removeCookie();
-        ?>
-    </body>
+</form>
+<?php
+if($_POST && isset($_POST['logout'])) removeCookie();
+?>
+</body>
 </html>
 <script>
     if ( window.history.replaceState ) {
@@ -60,21 +60,21 @@ $APIKey = "R0OsrTT4b64wOXbRAazkISyqoXbzWdsc";
 
 
 
-    function removeCookie():void{
-        setcookie("carquinyolisSession", null, time() - 3600);
-        redirectToLogin();
+function removeCookie():void{
+    setcookie("carquinyolisSession", null, time() - 3600);
+    redirectToLogin();
 
-    }
-    function redirectToLogin():void{
-        header("Location: /login.php");
-        header("Header2: Session Expired / Not logged in");
-        header("Header3: Redirecting to main page");
-    }
-    function searchGIF(string $input):void{
-        echo 'Search for " ' . $input . ' ":';
-        #codi crida api
-        #s'ha de protegir una session de session hijacking?
-    }
+}
+function redirectToLogin():void{
+    header("Location: /login.php");
+    header("Header2: Session Expired / Not logged in");
+    header("Header3: Redirecting to main page");
+}
+function searchGIF(string $input):void{
+    echo 'Search for " ' . $input . ' ":';
+    #codi crida api
+    #s'ha de protegir una session de session hijacking?
+}
 ?>
 <?php
 ob_end_flush();
